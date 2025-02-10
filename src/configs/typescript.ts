6abfec68d3c4def1148330551ca180ee60ac767c
Maston <22679886+mastondzn@users.noreply.github.com>
2025-02-10 14:05:14 +0200

    feat: add strict and stylistic rules to ts

--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -40,7 +40,83 @@ export async function typescript(
   const tsconfigPath = options?.tsconfigPath ?? undefined;
   const isTypeAware = !!tsconfigPath;
 
+  const [pluginTs, parserTs] = await Promise.all([
+    interopDefault(import('@typescript-eslint/eslint-plugin')),
+    interopDefault(import('@typescript-eslint/parser')),
+  ] as const);
+
+  const rules: TypedFlatConfigItem['rules'] = {
+    ...renameRules(
+      pluginTs.configs['eslint-recommended'].overrides![0].rules!,
+      { '@typescript-eslint': 'ts' },
+    ),
+    ...renameRules(pluginTs.configs.strict.rules!, {
+      '@typescript-eslint': 'ts',
+    }),
+    ...renameRules(pluginTs.configs.stylistic.rules!, {
+      '@typescript-eslint': 'ts',
+    }),
+
+    'no-dupe-class-members': 'off',
+    'no-redeclare': 'off',
+    'no-use-before-define': 'off',
+    'no-useless-constructor': 'off',
+    'ts/ban-ts-comment': [
+      'error',
+      { 'ts-expect-error': 'allow-with-description' },
+    ],
+    'ts/consistent-type-definitions': ['error', 'interface'],
+    'ts/consistent-type-imports': [
+      'error',
+      {
+        disallowTypeAnnotations: false,
+        fixStyle: 'separate-type-imports',
+        prefer: 'type-imports',
+      },
+    ],
+
+    'ts/method-signature-style': ['error', 'property'], // https://www.totaltypescript.com/method-shorthand-syntax-considered-harmful
+    'ts/no-dupe-class-members': 'error',
+    'ts/no-dynamic-delete': 'off',
+    'ts/no-empty-object-type': ['error', { allowInterfaces: 'always' }],
+    'ts/no-explicit-any': 'off',
+    'ts/no-extraneous-class': 'off',
+    'ts/no-import-type-side-effects': 'error',
+    'ts/no-invalid-void-type': 'off',
+    'ts/no-non-null-assertion': 'off',
+    'ts/no-redeclare': ['error', { builtinGlobals: false }],
+    'ts/no-require-imports': 'error',
+    'ts/no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTaggedTemplates: true,
+        allowTernary: true,
+      },
+    ],
+    'ts/no-unused-vars': 'off',
+    'ts/no-use-before-define': [
+      'error',
+      { classes: false, functions: false, variables: true },
+    ],
+    'ts/no-useless-constructor': 'off',
+    'ts/no-wrapper-object-types': 'error',
+    'ts/triple-slash-reference': 'off',
+    'ts/unified-signatures': 'off',
+  };
+
   const typeAwareRules: TypedFlatConfigItem['rules'] = {
+    ...renameRules(
+      pluginTs.configs['eslint-recommended'].overrides![0].rules!,
+      { '@typescript-eslint': 'ts' },
+    ),
+    ...renameRules(pluginTs.configs['strict-type-checked'].rules!, {
+      '@typescript-eslint': 'ts',
+    }),
+    ...renameRules(pluginTs.configs['stylistic-type-checked'].rules!, {
+      '@typescript-eslint': 'ts',
+    }),
+
     'dot-notation': 'off',
     'no-implied-eval': 'off',
     'ts/await-thenable': 'error',
@@ -66,11 +142,6 @@ export async function typescript(
     'ts/switch-exhaustiveness-check': 'error',
     'ts/unbound-method': 'error',
   };
-
-  const [pluginTs, parserTs] = await Promise.all([
-    interopDefault(import('@typescript-eslint/eslint-plugin')),
-    interopDefault(import('@typescript-eslint/parser')),
-  ] as const);
 
   function makeParser(
     typeAware: boolean,
@@ -121,59 +192,7 @@ export async function typescript(
       files,
       name: 'antfu/typescript/rules',
       rules: {
-        ...renameRules(
-          pluginTs.configs['eslint-recommended'].overrides![0].rules!,
-          { '@typescript-eslint': 'ts' },
-        ),
-        ...renameRules(pluginTs.configs.strict.rules!, {
-          '@typescript-eslint': 'ts',
-        }),
-        'no-dupe-class-members': 'off',
-        'no-redeclare': 'off',
-        'no-use-before-define': 'off',
-        'no-useless-constructor': 'off',
-        'ts/ban-ts-comment': [
-          'error',
-          { 'ts-expect-error': 'allow-with-description' },
-        ],
-        'ts/consistent-type-definitions': ['error', 'interface'],
-        'ts/consistent-type-imports': [
-          'error',
-          {
-            disallowTypeAnnotations: false,
-            fixStyle: 'separate-type-imports',
-            prefer: 'type-imports',
-          },
-        ],
-
-        'ts/method-signature-style': ['error', 'property'], // https://www.totaltypescript.com/method-shorthand-syntax-considered-harmful
-        'ts/no-dupe-class-members': 'error',
-        'ts/no-dynamic-delete': 'off',
-        'ts/no-empty-object-type': ['error', { allowInterfaces: 'always' }],
-        'ts/no-explicit-any': 'off',
-        'ts/no-extraneous-class': 'off',
-        'ts/no-import-type-side-effects': 'error',
-        'ts/no-invalid-void-type': 'off',
-        'ts/no-non-null-assertion': 'off',
-        'ts/no-redeclare': ['error', { builtinGlobals: false }],
-        'ts/no-require-imports': 'error',
-        'ts/no-unused-expressions': [
-          'error',
-          {
-            allowShortCircuit: true,
-            allowTaggedTemplates: true,
-            allowTernary: true,
-          },
-        ],
-        'ts/no-unused-vars': 'off',
-        'ts/no-use-before-define': [
-          'error',
-          { classes: false, functions: false, variables: true },
-        ],
-        'ts/no-useless-constructor': 'off',
-        'ts/no-wrapper-object-types': 'error',
-        'ts/triple-slash-reference': 'off',
-        'ts/unified-signatures': 'off',
+        ...rules,
         ...overrides,
       },
     },
