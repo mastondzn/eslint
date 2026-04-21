b1f6831e2d52b0edbc92260a5765d4e621ca6409
Maston <22679886+mastondzn@users.noreply.github.com>
2026-04-21 19:34:29 +0300

    refactor: simplify imports in typegen script

--- scripts/typegen.ts
+++ scripts/typegen.ts
@@ -3,62 +3,21 @@ import { writeFile } from 'node:fs/promises';
 import { flatConfigsToRulesDTS } from 'eslint-typegen/core';
 import { builtinRules } from 'eslint/use-at-your-own-risk';
 
-import {
-  combine,
-  comments,
-  imports,
-  javascript,
-  jsdoc,
-  jsonc,
-  jsx,
-  markdown,
-  node,
-  perfectionist,
-  react,
-  regexp,
-  sortPackageJson,
-  tailwindcss,
-  test,
-  toml,
-  typescript,
-  unicorn,
-  yaml,
-} from '../src';
+import { combine } from '../src';
 
 const configs = await combine(
   {
     plugins: {
-      '': {
-        // eslint-disable-next-line ts/no-deprecated
-        rules: Object.fromEntries(builtinRules.entries()),
-      },
+      // eslint-disable-next-line ts/no-deprecated
+      '': { rules: Object.fromEntries(builtinRules.entries()) },
     },
   },
-  comments(),
-  imports(),
-  javascript(),
-  jsx(),
-  jsdoc(),
-  jsonc(),
-  markdown(),
-  node(),
-  perfectionist(),
-  react(),
-  sortPackageJson(),
-  test(),
-  toml(),
-  regexp(),
-  typescript(),
-  unicorn(),
-  tailwindcss(),
-  yaml(),
+  ...(await import('../src/configs').then((m) => Object.values(m).map(async (fn) => fn()))),
 );
 
 const configNames = configs.map((i) => i.name).filter(Boolean) as string[];
 
-let dts = await flatConfigsToRulesDTS(configs, {
-  includeAugmentation: false,
-});
+let dts = await flatConfigsToRulesDTS(configs, { includeAugmentation: false });
 
 dts += `
 // Names of all the configs
